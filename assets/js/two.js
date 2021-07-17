9e22245e420cf20733d6826211d84519087e9359
Lucho Sorrentino <lucianomartin.sorrentino@gmail.com>
2021-07-16 21:13:05 -0300

    Idem anterior commit

---
+++ assets/js/two.js
@@ -0,0 +1,8 @@
+// Se necesita:
+// - Preguntarle al visitante(Desde cualquier página) si podemos enviarle un email con novedades
+// - Debemos guardar la respuesta para no tener que preguntar cada vez que ingrese.
+
+// Paginas: Todas
+// 0. Fijarse en localStorage si el cliente ya confirmó si quiere novedades
+// 1. Si no confirm si quiere recibir mails con novedades
+// 2. Guardar la respuesta en el localStorage

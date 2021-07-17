a59c859e5bade9c6d39379560071f5f87fcf50b6
Lucho Sorrentino <lucianomartin.sorrentino@gmail.com>
2021-07-17 02:18:59 -0300

    Agrego two.js

--- assets/js/two.js
+++ assets/js/two.js
@@ -3,6 +3,14 @@
 // - Debemos guardar la respuesta para no tener que preguntar cada vez que ingrese.
 
 // Paginas: Todas
-// 0. Fijarse en localStorage si el cliente ya confirmó si quiere novedades
-// 1. Si no confirm si quiere recibir mails con novedades
-// 2. Guardar la respuesta en el localStorage
+// 0. Fijarse en localStorage si el cliente ya confirmó si quiere novedades.
+// 1. Si no confirmó, preguntar si quiere recibir mails con novedades.
+// 2. Guardar la respuesta en el localStorage.
+
+if (!localStorage.getItem("recibirNovedades")) {
+
+    let recibirNovedades = confirm("¿Desea recibir novedades de nuestro sitio web en su correo-e?");
+    
+    localStorage.setItem("recibirNovedades", recibirNovedades );
+    
+}

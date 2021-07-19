ebc57bec82061610d30d042f46c57ea4619abccf
Jicaffo <jicaffo@yahoo.com>
2021-07-18 21:05:56 -0300

    Delay en funciones, y ajustes estéticos

--- assets/js/one.js
+++ assets/js/one.js
@@ -1,44 +1,41 @@
-/*
-- Cuando el usuario ingrese al sitio (desde cualquier página) debemos preguntarle si desea ingresar su nombre y su email. Debemos guardar esa información localmente.
-- Validar que no ingrese datos vacíos. En el caso del email, solo validar que lo ingresado contenga "." y "@"
-- Debemos recordar su decisión para no volver a preguntarle cada vez que ingrese.
-*/
+function preguntarGuardarDatos(){
 
+    if ( !localStorage.getItem("guardarDatos") ){
 
-if ( !localStorage.getItem("guardarDatos") ){
-
-    let guardarDatos = confirm("¿Le gustaría registrarse en nuestro portal?");
-
-    localStorage.setItem("guardarDatos", guardarDatos);   
-
-    if (guardarDatos){
-
-        let nombre = "";
+        let guardarDatos = confirm("¿Le gustaría registrarse en nuestro portal?");
     
-        while( nombre === "" || nombre === null ){
-
-            nombre = prompt("Ingrese su nombre");
-
-            if ( nombre === "" || nombre === null ){
-                alert("Ingrese un nombre válido.")
-            }
-        }
-
-        localStorage.setItem("nombre", nombre);
-
-        let mail = "";
+        localStorage.setItem("guardarDatos", guardarDatos);   
+    
+        if (guardarDatos){
+    
+            let nombre = "";
         
-        while( mail === "" || mail === null || !mail.includes(".") || !mail.includes("@") ){
-
-            mail = prompt("Ingrese su dirección de e-mail");
-
-            if ( mail === "" || mail === null || !mail.includes(".") || !mail.includes("@") ){
-
-                alert("Ingrese un e-mail válido.")
+            while( nombre === "" || nombre === null ){
+    
+                nombre = prompt("Ingrese su nombre");
+    
+                if ( nombre === "" || nombre === null ){
+                    alert("Ingrese un nombre válido.")
+                }
             }
+    
+            localStorage.setItem("nombre", nombre);
+    
+            let mail = "";
+            
+            while( mail === "" || mail === null || !mail.includes(".") || !mail.includes("@") ){
+    
+                mail = prompt("Ingrese su dirección de e-mail");
+    
+                if ( mail === "" || mail === null || !mail.includes(".") || !mail.includes("@") ){
+    
+                    alert("Ingrese un e-mail válido.")
+                }
+            }
+    
+            localStorage.setItem("mail", mail);
         }
-
-        localStorage.setItem("mail", mail);
     }
 }
 
+setTimeout(preguntarGuardarDatos,1000);

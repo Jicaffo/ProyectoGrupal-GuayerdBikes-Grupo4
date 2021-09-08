1c3f9859a02f0e27fa0ee6cb126865b47f9ed407
Lucho Sorrentino <lucianomartin.sorrentino@gmail.com>
2021-09-08 18:36:19 -0300

    Commit fetch imagen banner

--- assets/js/dos_traerBanner.js
+++ assets/js/dos_traerBanner.js
@@ -1,31 +1,42 @@
-// fetch("http://demo2420474.mockable.io/getHomeBanner")
-// 	.then(response => {
-//         console.log(response);
-       
-// 	})
-// imgURL: https://dummyimage.com/1600x351/000000/ffff&text=Otro+Banner+Guayerd
-
-const reqBanner = new XMLHttpRequest();
-const method = 'GET';
-const address = 'http://demo2420474.mockable.io/getHomeBanner';
 const banner = document.querySelector("#ancho-imagen-banner");
-let infoBanner;
 
-reqBanner.onreadystatechange = e => {
-        if (reqBanner.readyState == 4) {
-            if (reqBanner.status == 200) {
-                console.log(reqBanner)
-                infoBanner = JSON.parse(reqBanner.responseText)
-                console.log(infoBanner.imgUrl)
-                banner.src = infoBanner.imgUrl
-                banner.alt = infoBanner.title
+// let infoBanner;
+// const reqBanner = new XMLHttpRequest();
+// const method = 'GET';
+// const address = 'http://demo2420474.mockable.io/getHomeBanner';
+
+// reqBanner.onreadystatechange = e => {
+//         if (reqBanner.readyState == 4) {
+//             if (reqBanner.status == 200) {
+//                 console.log(reqBanner)
+//                 infoBanner = JSON.parse(reqBanner.responseText)
+//                 console.log(infoBanner.imgUrl)
+//                 banner.src = infoBanner.imgUrl
+//                 banner.alt = infoBanner.title
                 
-            } else if (reqBanner.status === 404) {
-                    console.log("Error");
-            } else {
+//             } else if (reqBanner.status === 404) {
+//                     console.log("Error");
+//             } else {
     
-            }
-        } 
-}
-reqBanner.open(method, address);
-reqBanner.send();
+//             }
+//         } 
+// }
+// reqBanner.open(method, address);
+// reqBanner.send();
+
+
+const isResponseOk = response => {
+    if (!response.ok)
+        throw new Error(response.status);
+    return response.json()    
+    }
+
+fetch('https://demo2420474.mockable.io/getHomeBanner')
+    .then( response => isResponseOk(response))
+    .then( data => {
+        console.log(data.imgUrl);
+        console.log(data.link);
+        console.log(data.title);
+        banner.src = data.imgUrl;
+        banner.alt = data.title;
+    })
